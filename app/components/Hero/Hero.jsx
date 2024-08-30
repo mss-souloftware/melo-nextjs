91f4a2d7348ec95b57863817b4765cc67f61963f
mss-souloftware <sufyan.souloftware@gmail.com>
2024-08-30 18:07:42 +0500

    project setup

---
+++ app/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <section>
+            <div className="container mx-auto">
+                <h1 className='text-black text-4xl'>BECAUSE BEING SICK SUCKS</h1>
+            </div>
+        </section>
+    )
+}
