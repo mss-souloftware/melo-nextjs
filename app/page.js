7a24fcd0b44b186d9fdf6671b3bedffdc6ba45b1
mss-souloftware <sufyan.souloftware@gmail.com>
2024-09-02 15:18:05 +0500

    build error

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/Hero";
 import Header from "./components/Header/Header";
 import Canvas from "./components/Canvas";
 
